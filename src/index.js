07a7c0b45e6f6f7b53a96f4726eca2ef403f2e9a
FilipKalicanin <filipkalicanin@hotmail.com>
2021-03-18 14:28:14 +0100

    TASK-LINKS-4, corrected names

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { instanceOfMainClass } from './mainClass';
-import { renderCategories, renderLinks, searchBarCategories, searchBarLinks, btnClearForCategories, btnClearForLinks } from "./displayUI";
+import { renderCategories, renderLinks, searchBarCategories, searchBarLinks, btnClearForCategoriesFilterText, btnClearForLinksFilterText } from "./displayUI";
 
 instanceOfMainClass.setOnCategoriesReceived(renderCategories);
 instanceOfMainClass.setOnLinksReceived(renderLinks);
@@ -11,5 +11,5 @@ instanceOfMainClass.collectAllLinks();
 
 searchBarCategories();
 searchBarLinks();
-btnClearForCategories();
-btnClearForLinks();
+btnClearForCategoriesFilterText();
+btnClearForLinksFilterText();
